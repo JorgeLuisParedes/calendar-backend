0d06eb6c6d8fb0272e84bc5a5fdc45df1311455f
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-01-24 20:42:15 +0100

    configurando express

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hola mundo");
+const express = require("express");
+
+// Crear el servidor de express
+const app = express();
+
+// Rutas
+app.get("/", (req, res) => {
+  console.log("Se requiere el /");
+  res.json({
+    ok: true,
+  });
+});
+
+// Escuchar peticiones
+app.listen(1138, () => {
+  console.log(`Servidor corriendo en el puerto ${1138}`);
+});
