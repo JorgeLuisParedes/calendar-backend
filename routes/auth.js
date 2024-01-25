99b835c3d34b7cafd5ada1e1bd8c50df346774f5
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-01-25 19:50:01 +0100

    express-validator

--- routes/auth.js
+++ routes/auth.js
@@ -5,11 +5,29 @@
 
 const { Router } = require('express');
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+const { check } = require('express-validator');
 const router = Router();
 
-router.post('/new', crearUsuario);
+router.post(
+	'/new',
+	[
+		// middelwares
+		check('name', 'El nombre es obligatorio').not().isEmpty(),
+		check('email', 'El email es obligatorio').isEmail(),
+		check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+	],
+	crearUsuario,
+);
 
-router.post('/', loginUsuario);
+router.post(
+	'/',
+	[
+		// middelwares
+		check('email', 'El email es obligatorio').isEmail(),
+		check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+	],
+	loginUsuario,
+);
 
 router.get('/renew', revalidarToken);
 
