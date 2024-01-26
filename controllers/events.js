ba42494cbcc5d256f772cf23ce27cd0ed038c6fe
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-01-26 11:50:35 +0100

    modelo evento

--- controllers/events.js
+++ controllers/events.js
@@ -8,6 +8,9 @@ const getEventos = (req, res = response) => {
 };
 
 const crearEvento = (req, res = response) => {
+	// Verficar que tenga el evento
+	console.log(req.body);
+
 	res.json({
 		ok: true,
 		msg: 'crearEvento',
