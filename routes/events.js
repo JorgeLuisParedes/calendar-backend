9515f549cfdc084c36d630e1b014023a400154d3
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-01-26 12:13:04 +0100

    validar campos necesarios

--- routes/events.js
+++ routes/events.js
@@ -12,6 +12,9 @@ const {
 	eliminarEvento,
 } = require('../controllers/events');
 const { validarJWT } = require('../middlewars/validar-jwt');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewars/validar-campos');
+const { isDate } = require('../helpers/isDate');
 
 // Todas tienen que pasar por la validación dek JWT
 router.use(validarJWT);
@@ -20,7 +23,16 @@ router.use(validarJWT);
 router.get('/', getEventos);
 
 // Crear eventos
-router.post('/', crearEvento);
+router.post(
+	'/',
+	[
+		check('title', 'El titulo es obligatorio').not().isEmpty(),
+		check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+		check('end', 'Fecha de finalización es obligatoria').custom(isDate),
+		validarCampos,
+	],
+	crearEvento,
+);
 
 // Actualizar eventos
 router.put('/:id', actualizarEvento);
